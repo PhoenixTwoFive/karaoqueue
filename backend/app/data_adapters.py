27eb10d222a84dcb8293ccfd895c23ebd514f1ed
Phillip Kühne <phillipkhne@gmail.com>
2020-05-26 16:07:29 +0200

    Remove Flask-based backend

--- backend/app/data_adapters.py
+++
@@ -1,8 +0,0 @@
-def dict_from_row(row):
-    return dict(zip(row.keys(), row))
-
-def dict_from_rows(rows):
-    outlist=[]
-    for row in rows:
-        outlist.append(dict_from_row(row))
-    return outlist
